d7ee171d9ded421575a1ece464caa354765fb3ca
btozel <btozel@gmail.com>
2024-08-26 14:53:07 -0700

    Adding cactus objects to the game

--- MovingElements.java
+++ MovingElements.java
@@ -1,12 +1,16 @@
 import java.awt.*;
 
 public class MovingElements extends GameElements{
+    private int movingElementVelocity;
+
+
     MovingElements(int x, int y, int width, int height, Image img) {
         super(x, y, width, height, img);
+        movingElementVelocity = 10;
     }
 
 
     public void move() {
-
+        xPosition -= movingElementVelocity;
     }
 }
